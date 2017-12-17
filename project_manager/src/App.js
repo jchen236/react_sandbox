773a011610c48a89a3d8f617916a2d20fcc759ac
Jimmy Chen <jzc8@cornell.edu>
2017-12-16 19:57:22 -0500

    Add project items

--- project_manager/src/App.js
+++ project_manager/src/App.js
@@ -1,11 +1,32 @@
 import React, { Component } from 'react';
 import './App.css';
+import Projects from './Components/Projects';
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      projects: [
+        {
+          title: 'Interactive UX',
+          category: 'Information Science'
+        },
+        {
+          title: 'Database Architecture',
+          category: 'CS'
+        },
+        {
+          title: 'Compilers',
+          category: 'CS'
+        }
+      ]
+    }
+  }
   render() {
     return (
       <div className="App">
         My App
+        <Projects projects={this.state.projects} />
       </div>
     );
   }
